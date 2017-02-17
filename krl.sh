cca7c78adf4157c40d7f3f3e848e866131d5df11
Pxlbin <willblew@gmail.com>
2017-02-17 21:07:30 +0000

    Fixes v2

--- krl.sh
+++ krl.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #
 [ $# -ge 1 -a "$1" ] && input="$1" || read input
-link=`curl -X POST --data-urlencode "url=$input" http://krl.io/create.php 2>/dev/null | grep textarea | tr '>' '\n' | tr '<' '\n' | grep http`
+link=`curl --silent --data "" http://krl.io/v1/link/$1 | tr -d '"'`
 echo -ne $link
+echo ""
