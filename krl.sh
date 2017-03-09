e59f5fdb0979f1be8dead9775d37739e106141bb
Abe Massry <amassry@linode.com>
2017-03-09 14:06:53 -0500

    remove debugging line

--- krl.sh
+++ krl.sh
@@ -2,6 +2,5 @@
 #
 [ $# -ge 1 -a "$1" ] && input="$1" || read input
 newInput=$(echo $input | sed -e 's/^http:\/\///g' -e 's/^https:\/\///g')
-echo $newInput
 link=`curl --silent --data "" http://krl.io/v1/link/$newInput | tr -d '"' | sed 's|{url:||g' | tr -d '}'`
 echo -ne $link
